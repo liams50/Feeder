20a1a73de3e33eb930b9187fed8ff722f20770d4
LiamStevens <liams50@nycstudents.net>
2025-09-11 15:34:41 -0400

    Wrote most of code, need to finish test cases in main

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
+        Feeder f = new Feeder(250);
+        System.out.println(f.simulateManyDays(10, 5) + "day(s)");
+        f = new Feeder(0);
+
     }
 }
